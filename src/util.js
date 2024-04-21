89e0c822fc6c0d58e7a57ed1f7a8affc05113815
Sonny Piers <sonny@fastmail.net>
2024-04-21 12:37:35 +0200

    Only restore window dimensions if width and height are truthy

--- src/util.js
+++ src/util.js
@@ -81,6 +81,11 @@ export function persistWindowState({ settings, window }) {
   }, 300);
   window.connect("notify::default-width", onSizeChanged);
   window.connect("notify::default-height", onSizeChanged);
-  window.default_width = settings.get_int("width");
-  window.default_height = settings.get_int("height");
+  const width = settings.get_int("width")
+  const height = settings.get_int("height")
+  if (width && height) {
+    window.default_width = width;
+    window.default_height = height;
+  }
 }
+
